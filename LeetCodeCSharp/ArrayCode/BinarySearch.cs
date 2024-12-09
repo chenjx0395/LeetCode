cb979d4c5f3fe549bd8931ba2a6640980ccb186a
陈序源 <chenjx0395@163.com>
2024-12-09 09:23:18 +0800

    add：二分查找

---
+++ LeetCodeCSharp/ArrayCode/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.ArrayCode
+{
+    internal class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+            //左闭右开思路
+            int lP = 0;
+            int rP = nums.Length;
+            int middle = (lP + rP) >>> 1;
+            while (lP < rP) {
+                if (target == nums[middle]) { 
+                    return middle;
+                }else if (target > nums[middle])
+                {
+                    lP = middle + 1;
+                }else
+                {
+                    rP = middle;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetCodeCSharp.ArrayCode;
+
+
+// 1.二分查找
+Case01();
+void Case01()
+{
+    int[] arr01 = { -1, 0, 3, 5, 9, 12 };
+    int target = 9;
+    int result = BinarySearch.Search(arr01, target);
+    Console.WriteLine(result);
+}
