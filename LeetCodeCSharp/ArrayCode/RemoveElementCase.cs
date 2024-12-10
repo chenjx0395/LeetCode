4789f6778579bd7a18befd78e81c036c34712308
陈序源 <chenjx0395@163.com>
2024-12-10 11:06:06 +0800

    feat：移除元素

---
+++ LeetCodeCSharp/ArrayCode/RemoveElementCase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp.ArrayCode
+{
+    internal class RemoveElementCase
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int length = nums.Length;
+            int count = 0;
+            int[] record = new int[length];
+            int left = 0, right = length - 1;
+            // 遍历数组
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    record[left++] = i;
+                    count++;
+                }
+                else
+                {
+                    record[right--] = i;
+                }
+            }
+            left = 0;
+            right = length - 1;
+            // 每次交换应交换目标值索引数组的最小值
+            // 交换元素
+            for (int i = 0; i < length - count; i++)
+            {
+                int leftMinIndex = NumsMin(record, count);
+                if (record[right] > record[leftMinIndex])
+                {
+                    nums[record[leftMinIndex]] = nums[record[right]];
+                    record[leftMinIndex] = record[right];
+                }
+                right--;
+            }
+            return count;
+        }
+
+        // 找出数组最小值的索引
+        private static int NumsMin(int[] nums, int length)
+        {
+            int minIndex = 0;
+            for (int i = 1; i < length; i++)
+            {
+                if (nums[i] < nums[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+            return minIndex;
+        }
+    }
+}
--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -1,8 +1,18 @@
 ﻿using LeetCodeCSharp.ArrayCode;
 
+//2.移除元素
+
+Case02();
+void Case02()
+{
+    int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+    int value = 2;
+    RemoveElementCase.RemoveElement(nums, value);
+}
+
 
 // 1.二分查找
-Case01();
+//Case01();
 void Case01()
 {
     int[] arr01 = { -1, 0, 3, 5, 9, 12 };
