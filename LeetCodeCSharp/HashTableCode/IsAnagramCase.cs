bf8cbf122c6f920dad50cdc2ae0c539a887d1ea2
陈序源 <chenjx0395@163.com>
2024-12-24 13:25:15 +0800

    哈希表：有效的字母异位词

---
+++ LeetCodeCSharp/HashTableCode/IsAnagramCase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace HashTableCode
+{
+    public class IsAnagramCase
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            var sChars = s.ToCharArray();
+            var tChars = t.ToCharArray();
+            if (sChars.Length != tChars.Length)
+                return false;
+            var dictionary = new Dictionary<char, int>();
+            
+            foreach (var tChar in tChars)
+            {
+                dictionary.TryGetValue(tChar, out var value);
+                dictionary[tChar] = value + 1;
+            }
+            foreach (var sChar in sChars)
+            {
+                dictionary.TryGetValue(sChar, out var value);
+                if (value == 0)
+                {
+                    return false;
+                }
+                dictionary[sChar] = value - 1;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCodeCSharp/HashTableCode/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace HashTableCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
